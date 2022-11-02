a52215269ab9d0e20308f68d56e745b5477b06f6
GUchechi <godswilluchendu7@gmail.com>
2022-11-02 09:06:41 +0100

    quotes app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useEffect } from 'react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
   const [quotes, setQuotes] = useState('')
+  const textRef = useRef(null)
+
+  let colors = ['#fff00', '#90ee90', '#ffa500', '#ff68ff', '#a9a9e7']
 
   const getQuote = async () => {
     const response = await fetch('https://type.fit/api/quotes')
@@ -15,10 +18,16 @@ function App() {
   useEffect(() => {
       getQuote();
   },[])
+
+  useEffect(() => {
+    textRef.current.style.color=
+    colors[Math.floor(Math.random() * colors.length)]
+  }, [quotes])
+
   return (
     <div className="App">
         <div className="quote">
-          <p>{quotes.text}</p>
+          <p ref={textRef}>{quotes.text}</p>
           <p>Author: {quotes.author}</p>
           <div className="btnContainer">
             <button onClick={getQuote} className="btn">Get Quote</button>
