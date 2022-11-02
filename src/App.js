381606178372bcedc15d75bb6a1d0d8f3367eed5
GUchechi <godswilluchendu7@gmail.com>
2022-11-02 08:58:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <div className="App">
         <div className="quote">
           <p>{quotes.text}</p>
-          <p>{quotes.author}</p>
+          <p>Author: {quotes.author}</p>
           <div className="btnContainer">
             <button onClick={getQuote} className="btn">Get Quote</button>
             <a 
